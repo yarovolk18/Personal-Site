56d529f7337afc22f07945be4b9ea48b1765439f
Iaroslav Volkov <iaroslavv18@gmail.com>
2024-04-08 21:29:58 -0400

    Buggin'

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/CV_IaroslavVolkov.pdf'
+import CV from '../../assets/2024_IaroslavVolkov.pdf'
 
 export const CTA = () => {
   return (
     <div className = 'cta'>
-        <a href = {CV} download className = 'btn'> Download CV</a>
+        <a href = {CV} download className = 'btn'> Download Resume</a>
         <a href = "#contact" className = 'btn btn-primary'> Let's Talk</a>
         </div>
   )
